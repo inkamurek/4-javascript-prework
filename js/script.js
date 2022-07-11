6fd34558ea6ef3492f237fcadc712b70bffcf0de
inkamurek <zurekkinga@gmail.com>
2022-07-11 12:10:25 +0200

    Add playerMove to JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+// ruch komputera
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 
@@ -14,3 +17,24 @@ if (randomNumber == '1') {
 }
 
 printMessage('Mój ruch: ' + computerMove);
+
+
+
+
+// ruch gracza
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage('Nie ma takiej opcji! Załóżmy, że ... ');
+  playerMove = ' kamień!';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
